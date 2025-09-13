cd2d0fc998bef55033e7ff51eec10824f3430009
thdft <thdft@users.noreply.github.com>
2025-09-13 15:40:26 -0300

    indicators: crossover

--- src/qfin/indicators/common.py
+++ src/qfin/indicators/common.py
@@ -48,3 +48,63 @@ def revert_echo(dataserie, empty_value=None):
 
         prev = value
     return arr
+
+
+# ----------------
+#  crossover
+# ----------------
+
+
+def crossover(dataserie_a, dataserie_b, shift=1, echo=False, nosignal_value=0):
+    """whether a variable series crossed over another series"""
+
+    col = "cross"
+    df = dataserie_a.to_frame().copy()
+    df["a"] = dataserie_a
+    df["b"] = dataserie_b
+    df[col] = np.select(
+        [
+            (df["a"] > df["b"]) & (df["a"].shift(shift) < df["b"].shift(shift)),
+            (df["a"] < df["b"]) & (df["a"].shift(shift) > df["b"].shift(shift)),
+        ],
+        [1, -1],
+        nosignal_value,
+    )
+
+    if echo:
+        df[col] = continue_echo(df[col], initial_value=0, skip_values=[nosignal_value])
+
+    return df[col]
+
+
+# -------------------------
+#  cross with 3 dataseries
+# -------------------------
+
+crossover3_labels = {3: "bullish", 2: "accumulation", 1: "recovery", -1: "warning", -2: "distribution", -3: "bearish"}
+
+
+def crossover3(dataserie_a, dataserie_b, dataserie_c, echo=False, nosignal_value=0):
+    col = "cross"
+    df = dataserie_a.to_frame().copy()
+    df["a"] = dataserie_a
+    df["b"] = dataserie_b
+    df["c"] = dataserie_c
+
+    df[col] = np.select(
+        [
+            (df["a"] < df["b"]) & (df["a"] > df["c"]) & (df["b"] > df["c"]),
+            (df["a"] < df["b"]) & (df["a"] < df["c"]) & (df["b"] > df["c"]),
+            (df["a"] < df["b"]) & (df["a"] < df["c"]) & (df["b"] < df["c"]),
+            (df["a"] > df["b"]) & (df["a"] < df["c"]) & (df["b"] < df["c"]),
+            (df["a"] > df["b"]) & (df["a"] > df["c"]) & (df["b"] < df["c"]),
+            (df["a"] > df["b"]) & (df["a"] > df["c"]) & (df["b"] > df["c"]),
+        ],
+        [-1, -2, -3, 1, 2, 3],
+        nosignal_value,
+    )
+
+    if not echo:
+        df[col] = revert_echo(df[col], empty_value=nosignal_value)
+
+    return df[col]
